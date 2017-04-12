aefa90de5408c656444a3a7009bfa8d2dbd64435
byung rae kim <byung rae kim>
2017-04-12 13:20:43 +0900

    step01

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import path from 'path';
-
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
 let app = express();
+
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
